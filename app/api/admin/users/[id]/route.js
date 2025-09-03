3062099d6589bcd0548090fe42425ecb6b690f51
LuisDiaz1234 <131550504+LuisDiaz1234@users.noreply.github.com>
2025-09-03 16:08:13 -0600

    Update route.js

--- app/api/admin/users/[id]/route.js
+++ app/api/admin/users/[id]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { getSupabaseAdmin } from '../../../../lib/supabaseAdmin';
+import { getSupabaseAdmin } from '../../../../../lib/supabaseAdmin';
 
 
 async function assertAdmin(req, supabaseAdmin) {
