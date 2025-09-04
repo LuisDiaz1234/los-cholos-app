0d3a93e96bca492d2e159fa20791e7af804b0991
LuisDiaz1234 <131550504+LuisDiaz1234@users.noreply.github.com>
2025-09-04 09:02:43 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useMemo, useState } from 'react';
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 import { useRequireAuth } from '@/lib/useRequireAuth';
 
 // ====== Config ======
