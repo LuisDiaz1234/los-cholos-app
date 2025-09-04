24a9db3d3534b0157ad521a5dd29d9a64834847c
LuisDiaz1234 <131550504+LuisDiaz1234@users.noreply.github.com>
2025-09-04 13:05:02 -0600

    Update error.js

--- app/error.js
+++ app/error.js
@@ -4,7 +4,7 @@ export default function Error({ error, reset }) {
   return (
     <div className="card">
       <h2>Ups, algo salió mal</h2>
-      <div className="alert err" style={{whiteSpace:'pre-wrap'}}>
+      <div className="alert err" style={{ whiteSpace:'pre-wrap' }}>
         {error?.message || 'Error inesperado.'}
       </div>
       <button className="btn" onClick={() => reset()}>Reintentar</button>
